455d7099d4fbc7b23e98fb29f994b310c94ffdc9
Maxime Bacoux <max@bacoux.com>
2022-01-08 19:54:54 +0100

    Add loader

--- include/resin/resin.h
+++ include/resin/resin.h
@@ -1,4 +1,12 @@
 #ifndef RESIN_RESIN_H
 #define RESIN_RESIN_H
 
-#endif /* RESIN_RESIN_H */
+#if defined(__cplusplus)
+# define RESIN_API extern "C"
+#else
+# define RESIN_API
+#endif
+
+RESIN_API void* resin_GetProcAddr(const char* name);
+
+#endif
